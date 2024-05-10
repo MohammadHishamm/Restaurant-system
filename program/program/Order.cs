2cc60a73abce4de8218c23d0c327151abc1ccfcb
MohammadHishamm <mohammad2109652@miuegypt.edu.eg>
2024-05-10 16:03:37 +0300

    order class

--- program/program/Order.cs
+++ program/program/Order.cs
@@ -10,25 +10,42 @@ namespace program
     {
         public int OrderID { get; private set; }
         public string Status { get; set; }
-        private List<string> menuItems;
+        public List<MenuItem> MenuItems { get; private set; }
 
-      
         public Order(int orderID, string status)
         {
             OrderID = orderID;
             Status = status;
-            menuItems = new List<string>();
+            MenuItems = new List<MenuItem>();
         }
 
-
-        public void AddMenuItem(string menuItem)
+        public void AddMenuItem(MenuItem menuItem)
         {
-            menuItems.Add(menuItem);
+
+            MenuItems.Add(menuItem);
+            Console.WriteLine($"Added '{menuItem.Title}' to order {OrderID}.");
         }
 
-        public void RemoveMenuItem(string menuItem)
+        public void RemoveMenuItem(MenuItem menuItem)
         {
-            menuItems.Remove(menuItem);
+            if (MenuItems.Contains(menuItem))
+            {
+                MenuItems.Remove(menuItem);
+                Console.WriteLine($"Removed '{menuItem.Title}' from order {OrderID}.");
+            }
+            else
+            {
+                Console.WriteLine($"Item '{menuItem.Title}' is not in order {OrderID}. Cannot remove.");
+            }
+        }
+
+        public void ViewOrderDetails()
+        {
+            Console.WriteLine($"Order Details for Order {OrderID}:");
+            foreach (var item in MenuItems)
+            {
+                Console.WriteLine($"ID: {item.MenuItemID}, Title: {item.Title}, Description: {item.Description}, Price: {item.Price}");
+            }
         }
     }
 }
